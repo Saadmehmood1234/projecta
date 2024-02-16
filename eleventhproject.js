ac811302d819204ac6e5dd8be7eb948d0b5d8996
Saadmehmood1234 <158064428+Saadmehmood1234@users.noreply.github.com>
2024-02-16 07:28:02 -0800

    Add files via upload

---
+++ eleventhproject.js
@@ -0,0 +1,18 @@
+var selectfield=document.getElementById("selectfield");
+var selectText=document.getElementById("selectText");
+var options=document.getElementsByClassName("options");
+var list=document.getElementById("list");
+var arrowicon=document.getElementById("arrowicon");
+
+selectfield.onclick=function(){
+    list.classList.toggle("hide");
+    arrowicon.classList.toggle("rotate");
+}
+for(option of options){
+    option.onclick=function(){
+        selectText.innerHTML=this.textContent;
+        list.classList.toggle("hide");
+        arrowicon.classList.toggle("rotate");
+
+    }
+}
